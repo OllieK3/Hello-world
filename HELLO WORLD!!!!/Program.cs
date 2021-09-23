9b5c94ab6b0d9ba9b900e2857107f3a5538fb7e7
ESCG\522859 <522859>
2021-09-23 14:48:07 +0100

    Up to the lists

--- HELLO WORLD!!!!/Program.cs
+++ HELLO WORLD!!!!/Program.cs
@@ -1,12 +1,37 @@
 ﻿using System;
-
+using System.Collections.Generic;
 namespace HELLO_WORLD____
 {
     class Program
     {
+        static double Height;
+        static int age;
+        static string message = "My age is ";
+        static string HeightMessage = "My height is ";
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+           List<int> numberList = new List<int>() { 6, 12, 18, 24 };
+            for (int i = 0; i < numberList.Count; i++)
+            {
+                Console.WriteLine(numberList[i]);
+            }
+
+          List<string> WordList = new List<string>() { "The funny business", "The monkey business" };
+          for (int i = 0; i <WordList.Count; i++)
+
+            {
+                Console.WriteLine(WordList[i]);
+            }
+
+            age = 16;
+            Height = 1.1;
+            Console.WriteLine(message + age);
+            Console.WriteLine(HeightMessage + Height);
+            Console.ReadKey();
+            
+
+
+            
         }
     }
 }
